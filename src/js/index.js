cf3df9ad81e9291dc40691aff50a5038b8d95add
Rogerio Manoel <rj.prod@hotmail.com>
2023-03-11 23:43:06 -0300

    finalizando extrutura js

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,30 @@
 
-
+//pegando o elemento HTML dos botões
 const botoesCarrosel = document.querySelectorAll('.botao');
+const imagens = document.querySelectorAll('.imagem');
 
+//identificar click
+botoesCarrosel.forEach((botao, indice) => {
+    botao.addEventListener('click', () => {
+        // desmarcar botão selecioando anterior
+        destativarBotaoSelecionado();
+
+        //marcar o botão clicado como selecionado
+        botao.classList.add('selecionado');
+
+        //escondendo a imagem ativa
+        const imagemAtiva = document.querySelector('.ativa');
+        imagemAtiva.classList.remove('ativa');
+
+        //exibindo imagem correspondente ao botão clicado
+        imagens[indice].classList.add('ativa');
+    })
+})
+
+
+
+function destativarBotaoSelecionado() {
+    const botaoSelecionado = document.querySelector('.selecionado');
+    botaoSelecionado.classList.remove('selecionado');
+}
 
